840a840587e39c8bd68b23c69cd73f7dc8203c9b
smcarrera <scarrera@scarrera-macbookair.roam.corp.google.com>
2014-10-25 12:41:49 -0400

    smcarrera mashupV2 - with changes
    changes include:
    * changes to pass test (changed some lines to object.notation - was
    following WU API instructions when using brackets
    *simplification of some JS lines (removed a few unnecessary variables
    by consolidating lines)
    *wrapping all JS in a function to avoid creating global variables
    *addition of WU API call to pull time of day and JS to change
    background image based on time of day. (Note only the ‘Clear’ weather
    forecast has an actual different image for night hours at this moment)
    * addition of some weather images (based on current conditions
    yesterday & today)
    *some CSS cleanup, note a lot more needed

--- mashup.js
+++ mashup.js
@@ -1,15 +1,18 @@
 // JavaScript Document
 
-	
-///instagram	
-
+$(function() {	
+///instagram
+//returns the 16 most recent #brooklynbridge pictures	
       $.ajax({
         url: 	'https://api.instagram.com/v1/tags/brooklynbridge/media/recent?client_id=61e21cfee4754af6a970bc6fbb4d3363&count=16',
         dataType: 'jsonp',
         success: function(result){
           for (var i = 0; i < result.data.length; i++){
             var url = result.data[i].images.thumbnail.url;
+			var timearray = result.data[i].created_time;
             $('#id'+(i+1)).append('<img src="' + url + '"/>');
+			//I am including this following line to create a second table that will be the same size as the first to host the weather background image
+			//I suspect there is a better way to do this with CSS than having to host and hide images twice - for next version
 			$('#ids'+(i+1)).append('<img src="' + url + '"/>');
           }
         }
@@ -20,38 +23,91 @@
 
 
 //WU Underground
-var condsarray = {LightRain:'lightrain3.jpg', Rain:'lightrain3.jpg', Overcast:'overcast.jpg'};
+
+// this is an array containing images for different weather conditions during day light hours
+//note not all conditions are included yet*
+var condsarrayd = {Drizzle:'lightrain3.jpg', LightDrizzle:'lightrain3.jpg', HeavyDrizzle:'lightrain3.jpg', 
+      			FreezingDrizzle:'lightrain3.jpg', LightFreezingDrizzle:'lightrain3.jpg', HeavyFreezingDrizzle:'lightrain3.jpg',
+				Rain:'lightrain3.jpg', LightRain:'lightrain3.jpg', HeavyRain:'lightrain3.jpg', 
+				RainShowers:'lightrain3.jpg', LightRainShowers:'lightrain3.jpg', HeavyRainShowers:'lightrain3.jpg', 
+				RainMist:'lightrain3.jpg', LightRainMist:'lightrain3.jpg', HeavyRainMist:'lightrain3.jpg',
+				FreezingRain:'lightrain3.jpg', LightFreezingRain:'lightrain3.jpg', HeavyFreezingRain:'lightrain3.jpg', 
+				Fog:'fog.jpg', LightFog:'fog.jpg', HeavyFog:'fog.jpg',
+				FogPatches:'fog.jpg', LightFogPatches:'fog.jpg', HeavyFogPatches:'fog.jpg',
+				FreezingFog:'fog.jpg', LightFreezingFog:'fog.jpg', HeavyFreezingFog:'fog.jpg',
+				PatchesofFog:'fog.jpg', PartialFog:'fog.jpg', ShallowFog:'fog.jpg',
+				Overcast:'overcast.jpg', 
+				PartlyCloudy:'cloudy.jpg', MostlyCloudy:'cloudy.jpg', ScatteredCloudy:'cloudy.jpg',
+				Clear:'clear.jpg'};
+				
+				
+// this is an array containing images for different weather conditions during night hours
+// currently only the clear condition is different, in theory all would be different images taken at night	
+//note not all conditions are included yet*			
+var condsarrayn = {Drizzle:'lightrain3.jpg', LightDrizzle:'lightrain3.jpg', HeavyDrizzle:'lightrain3.jpg', 
+				FreezingDrizzle:'lightrain3.jpg', LightFreezingDrizzle:'lightrain3.jpg', HeavyFreezingDrizzle:'lightrain3.jpg',
+				Rain:'lightrain3.jpg', LightRain:'lightrain3.jpg', HeavyRain:'lightrain3.jpg', 
+				RainShowers:'lightrain3.jpg', LightRainShowers:'lightrain3.jpg', HeavyRainShowers:'lightrain3.jpg', 
+				RainMist:'lightrain3.jpg', LightRainMist:'lightrain3.jpg', HeavyRainMist:'lightrain3.jpg',
+				FreezingRain:'lightrain3.jpg', LightFreezingRain:'lightrain3.jpg', HeavyFreezingRain:'lightrain3.jpg', 
+				Fog:'fog.jpg', LightFog:'fog.jpg', HeavyFog:'fog.jpg',
+				FogPatches:'fog.jpg', LightFogPatches:'fog.jpg', HeavyFogPatches:'fog.jpg',
+				FreezingFog:'fog.jpg', LightFreezingFog:'fog.jpg', HeavyFreezingFog:'fog.jpg',
+				PatchesofFog:'fog.jpg', PartialFog:'fog.jpg', ShallowFog:'fog.jpg',
+				Overcast:'overcast.jpg', 
+				PartlyCloudy:'cloudy.jpg', MostlyCloudy:'cloudy.jpg', ScatteredCloudy:'cloudy.jpg',
+				Clear:'clearnight.jpg'};
 
 
-function backImage(obj) 
+//function to retrieve the image associated with a weather condition & time of day
+function backImage(obj, hour) 
 {
-      var img1 = condsarray[obj];
-      return(img1);    
+      //night time images
+	  if (hour<4 || hour>20){
+		  var img1 = condsarrayn[obj];
+		  if(obj =='Clear'){
+			  $('#ccontainer').css("color", "white");
+		  }
+		  return(img1);
+	  }
+	  
+	  //day time images
+	  else{
+		  var img1 = condsarrayd[obj];
+      	  return(img1);
+	  }
 }
 
+//WU API call
   $.ajax({
 	  url : "http://api.wunderground.com/api/214e7c6b6a047461/geolookup/conditions/q/NY/New_York.json",
 	  dataType : "jsonp",
 	  success : function(parsed_json) {
 	 //returns the current temperature
-	  var temp_f = parsed_json['current_observation']['temp_f'];
+	  var temp_f = parsed_json.current_observation.temp_f;
 	  $('#tempnow').append("Temperture: "+temp_f+" F ");
 	  
 	  //returns the weather condition
-	  var conds = parsed_json['current_observation']['weather'];
+	  var conds = parsed_json.current_observation.weather;
 	  var condsnosp = conds.replace( /\s/g, "");
 	  $('#condsnow').append("Conditions: "+conds);
 	  
-       //sets the background image associated with the weather condition
-	  var weatherimg = backImage(condsnosp);
-	  $('#weathert').css("background-image", "url("+weatherimg+")"); 
+	  //returns the time of the weather condition/request in format 'hour'
+	  //currently not clear on the difference between observation_time and local_time in WU API, both appear to be the same
+	  var obsarray = parsed_json.current_observation.observation_time_rfc822.split(/[ ,]+/);;
+	  var hour =  parseInt(obsarray[4]);
+	  
+       //adds the background image associated with the weather condition
+	  $('#weathert').css("background-image", "url("+backImage(condsnosp, hour)+")"); 
 	  
 	  
 	  }
   });
 
 
-////though about but did not end up using the following, may combine or refactor later
+////thought about but did not end up using the following, may combine or refactor later
+
+
 //google images	  
 /*	 $.ajax({
 	  url : "https://www.googleapis.com/customsearch/v1?key=AIzaSyBdM4FHTAyUqL_AVjwuFgxlHpTukRvV1k4&cx=YOUR_CSE_ID&q=brooklyn+bridge&searchType=image&fileType=jpg&imgSize=small&alt=json",
@@ -126,3 +182,4 @@ $.ajax({
         return collection;
     };*/
 /// });
+});
